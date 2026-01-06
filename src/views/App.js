3c98026306c4394af06b08b2a43c3557e9248a6e
Phạm Đình Nam <nampham050723@gmail.com>
2026-01-06 22:32:43 +0700

    ToDo App - Create/Read

--- src/views/App.js
+++ src/views/App.js
@@ -2,16 +2,34 @@ import logo from "./logo.svg";
 import "./App.scss";
 import "./Example/MyComponent";
 import MyComponent from "./Example/MyComponent";
+import ListTodo from "./Todos/ListTodo";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Bounce } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world with reactjs</p>
+        <p>Simple todo apps with reactjs</p>
 
-        <MyComponent />
+        {/* <MyComponent /> */}
+        <ListTodo />
       </header>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </div>
   );
 }
